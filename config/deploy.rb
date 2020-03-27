a650daba0701a508edc04a32d56e7d4ad984fca7
tosiran <f2.ree_eat_10470@icloud.com>
2020-03-27 17:09:41 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,11 +8,11 @@ set :application, 'chat-space'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:tosiran/chat-space.git'
 
-# secrets.yml用のシンボリックリンクを追加
-set :linked_files, %w{ config/secrets.yml }
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{config/secrets.yml}
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
